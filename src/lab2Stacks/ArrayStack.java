4bb9a060bddf7411bb279a9cdb5d710629557b07
John Li <lij19@wit.edu>
2019-09-26 09:46:59 -0400

    added ArrayStack

---
+++ src/lab2Stacks/ArrayStack.java
@@ -0,0 +1,35 @@
+package lab2Stacks;
+
+public class ArrayStack <T> implements StackInterface <T> {
+	private T[] stackArray;
+	private int stackLength;
+	private int capacity;
+	private static final int DEFAULT_CAPACITY = 50;
+
+
+	@Override
+	public void push(T anEntry) {
+		// TODO Auto-generated method stub
+
+	}
+	@Override
+	public T pop() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public T peek() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+
+	}
+}
