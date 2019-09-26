7326a5c0cdbc75d1a4e9990424409ccdd5c0495d
TechnoHound <ajkreuzkamp@gmail.com>
2019-09-26 09:48:35 -0400

    added the basics I found from the professor's lecture files

--- src/lab2Stacks/LinkedStack.java
+++ src/lab2Stacks/LinkedStack.java
@@ -1,5 +1,68 @@
 package lab2Stacks;
 
-public class LinkedStack {
-
-}
+public class LinkedStack <T> implements StackInterface <T>
+{
+	private Node firstNode;
+	
+	public LinkedStack()
+	{
+		firstNode = null;
+	}
+	public void push(T anEntry)
+	{
+		Node dataNode = new Node(anEntry);
+		dataNode.setNext(topNode);
+		topNode = dataNode;
+	}
+	public T pop()
+	{
+		return null;	// stub; to be implemented
+	}
+	public T peek()
+	{
+		return null;	// stub; to be implemented
+	}
+	
+	@Override
+	public boolean isEmpty()
+	{
+		return (topNode == null);
+	};
+	
+	public void clear()
+	{
+		topNode = null;	// not a stub, but the code is very simple
+	};
+	
+	private class Node
+	{
+		private T data;
+		private Node next;
+		
+		public Node( )
+		{
+			this (null);
+		}
+		public Node(T anEntry)
+		{
+			data = anEntry;
+			next = null;
+		}
+		public void setData (T dataPortion)
+		{
+			data = dataPortion;
+		}
+		public T getData()
+		{
+			return data;
+		}
+		public void setNext(Node nextNode)
+		{
+			next = nextNode;
+		}
+		public Node getNext()
+		{
+			return next;
+		}
+	} // END Node
+} // END LinkedStack
