ffcf98219d353cb792796b92b2964ba743a98067
John Li <lij19@wit.edu>
2019-09-27 10:45:15 -0400

    fixed the convert method within the class InfixToPostfix

--- src/lab2Stacks/InfixToPostfix.java
+++ src/lab2Stacks/InfixToPostfix.java
@@ -1,15 +1,62 @@
 package lab2Stacks;
 
 public class InfixToPostfix {
+	private static boolean checkBalance (String expression) { //part of the lab is to also make sure that there is no problem with the brackets
+        LinkedStack <Character> openDelimiterStack = new LinkedStack<>();
+        boolean isBalanced = true;
+        int index = 0;
+        int characterCount = expression.length();
+        char nextCharacter;
+        
+        while (isBalanced && index < characterCount){
+            nextCharacter = expression.charAt(index);
+            switch (nextCharacter)
+            {
+                case '(': case '[': case '{':
+                    openDelimiterStack.push(nextCharacter);
+                    break;
+                case ')': case ']': case '}':
+                    if (openDelimiterStack.isEmpty())
+                        isBalanced = false;
+                    else {
+                        char openDelimiter = openDelimiterStack.pop();
+                        isBalanced = 
+                              isPaired (openDelimiter, nextCharacter );
+                    }
+                    break;
+                default: //irelevant character
+                    break;
+                 
+            }
+            index ++;
+        }
+        if (!openDelimiterStack.isEmpty())
+            isBalanced = false;
+        
+        return isBalanced;
+    }
+	
+    private static boolean isPaired (char first, char second) {
+        return (first == '(' && second == ')' ||
+                first == '[' && second == ']' ||
+                first == '{' && second == '}');
+    }
+
 	public static String convert(String a) {
+		if(!checkBalance(a)) { //checks to make sure that everything actually closes
+			return "nope";
+		}
+		
 		LinkedStack<Character> operatorStack = new LinkedStack<>();
 		String postfix = "";
 
 		while (!a.isEmpty()) {
-			char nextChar = a.charAt(a.length() - 1);
-			a = a.substring(0, a.length() - 1);
-					switch (nextChar)
-					{
+			char nextChar = a.charAt(0);
+			a = a.substring(1, a.length());
+					switch (nextChar) {
+					case '0': // checks for numbers 0 to 9 variables
+						postfix += nextChar;
+						break;
 					case '1':
 						postfix += nextChar;
 						break;
@@ -17,6 +64,24 @@ public class InfixToPostfix {
 						postfix += nextChar;
 						break;
 					case '3':
+						postfix += nextChar;
+						break;
+					case '4':
+						postfix += nextChar;
+						break;
+					case '5':
+						postfix += nextChar;
+						break;
+					case '6':
+						postfix += nextChar;
+						break;
+					case '7':
+						postfix += nextChar;
+						break;
+					case '8':
+						postfix += nextChar;
+						break;
+					case '9':
 						postfix += nextChar;
 						break;
 					case '^':
@@ -44,7 +109,7 @@ public class InfixToPostfix {
 						operatorStack.push(nextChar);
 						break;
 					case '/' :
-						while (!operatorStack.isEmpty() && precedence(nextChar)<= precedence(operatorStack.peek())) {
+						while (!operatorStack.isEmpty() && precedence(nextChar) <= precedence(operatorStack.peek())) {
 							postfix += operatorStack.peek();
 							operatorStack.pop();
 						}
@@ -57,8 +122,7 @@ public class InfixToPostfix {
 						char topOperator = operatorStack.pop();
 						while (topOperator != '(') {
 							postfix += topOperator;
-							topOperator = operatorStack.pop();
-							
+							topOperator = operatorStack.pop();	
 						}
 						break;
 					default:
@@ -74,7 +138,7 @@ public class InfixToPostfix {
 		return postfix;
 	}
 	
-	private static int precedence(char c) {
+	private static int precedence(char c) { //based on the symbols, a number is returned representing how import said symbols are
 		switch(c) {
 		case '(':
 			return 1;
--- src/lab2Stacks/Test.java
+++ src/lab2Stacks/Test.java
@@ -2,7 +2,10 @@ package lab2Stacks;
 
 public class Test {
 	public static void main(String[] args) {
+		LinkedStack<Character> stack1 = new LinkedStack<>();
 		String a = "(1+2)*3";
+		System.out.println(a);
 		System.out.println(InfixToPostfix.convert(a));
+		
 	}
 }
