ae1f82f82b59006c3c3643f32c3364013ef1abff
John Li <lij19@wit.edu>
2019-09-27 12:47:47 -0400

    altered test class

--- src/lab2Stacks/Test.java
+++ src/lab2Stacks/Test.java
@@ -2,10 +2,9 @@ package lab2Stacks;
 
 public class Test {
 	public static void main(String[] args) {
-		LinkedStack<Character> stack1 = new LinkedStack<>();
 		String a = "(1+2)*3";
 		System.out.println(a);
-		System.out.println(InfixToPostfix.convert(a));
+		System.out.println(InfixToPostfix.convert(a)); //answer should 12+3*
 		
 	}
 }
