64528f6d786b06e5a97b91872186d71a8cb164a8
John Li <lij19@wit.edu>
2019-09-30 16:59:28 -0400

    added more to the driver class to test PostfixEvalutaor eval method

--- src/lab2Stacks/Driver.java
+++ src/lab2Stacks/Driver.java
@@ -6,7 +6,11 @@ public class Driver {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		
+		System.out.println("Input equation");
+		String a = input.next();
 		
+		System.out.println(InfixToPostfix.convert(a));
+		System.out.println(PostfixEvaluator.eval(InfixToPostfix.convert(a)));
 		
 		input.close();
 	}
