f73af3db0d5bd01df089d609930193a3889a001a
John Li <lij19@wit.edu>
2019-09-30 16:16:12 -0400

    added spaces

--- src/lab2Stacks/Driver.java
+++ src/lab2Stacks/Driver.java
@@ -1,7 +1,13 @@
 package lab2Stacks;
 
+import java.util.Scanner;
+
 public class Driver {
 	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
 		
+		
+		
+		input.close();
 	}
 }
