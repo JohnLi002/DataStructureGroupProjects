77467b58d8fd7e6dcb532aed405b7ba8e73b111e
John Li <lij19@wit.edu>
2019-10-01 16:35:08 -0400

    added conversion of character into proper number

--- src/lab2Stacks/PostfixEvaluator.java
+++ src/lab2Stacks/PostfixEvaluator.java
@@ -2,30 +2,31 @@ package lab2Stacks;
 
 public class PostfixEvaluator {
 	public static double eval(String a) {
-        LinkedStack<Double> valueStack = new LinkedStack<>();
-        while (!a.isEmpty()) { //Searches through the whole string.
-            char nextChar = a.charAt(0);
-            a = a.substring(1, a.length());
-            if('0' <= nextChar && nextChar <= '9') {
-                valueStack.push((double) nextChar); //Turns the character into double.
-            }else if(nextChar == '+'  || nextChar == '-' || nextChar == '*' || nextChar == '/' || nextChar == '^') {
-                double operandTwo = valueStack.pop();
-                double operandOne = valueStack.pop();
-                double result;
-                if(nextChar == '+') {
-                    result = operandOne + operandTwo;
-                }else if(nextChar == '-') {
-                    result = operandOne - operandTwo;
-                }else if(nextChar == '*') {
-                    result = operandOne * operandTwo;
-                }else if(nextChar == '/') {
-                    result = operandOne / operandTwo;
-                }else{ //This is for the exponent.
-                    result = Math.pow(operandOne, operandTwo);
-                }
-                    valueStack.push(result);
-            }
-        }
-        return valueStack.pop();
-    }
+		LinkedStack<Double> valueStack = new LinkedStack<>();
+		while (!a.isEmpty()) { //Searches through the whole string.
+			char nextChar = a.charAt(0);
+			a = a.substring(1, a.length());
+			if('0' <= nextChar && nextChar <= '9') {
+				valueStack.push((double) (nextChar - '0')); //Turns the character into double.
+			}else if(nextChar == '+'  || nextChar == '-' || nextChar == '*' || nextChar == '/' || nextChar == '^') {
+				double operandTwo = valueStack.pop();
+				double operandOne = valueStack.pop();
+				
+				double result;
+				if(nextChar == '+') {
+					result = operandOne + operandTwo;
+				}else if(nextChar == '-') {
+					result = operandOne - operandTwo;
+				}else if(nextChar == '*') {
+					result = operandOne * operandTwo;
+				}else if(nextChar == '/') {
+					result = operandOne / operandTwo;
+				}else{ //This is for the exponent.
+					result = Math.pow(operandOne, operandTwo);
+				}
+				valueStack.push(result);
+			}
+		}
+		return valueStack.pop();
+	}
 }
