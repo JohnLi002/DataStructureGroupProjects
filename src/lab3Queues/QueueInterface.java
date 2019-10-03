08d68a71b79da8632e54aad557a433e2135f5ddf
John Li <lij19@wit.edu>
2019-10-03 09:56:30 -0400

    Created Lab3 folder

---
+++ src/lab3Queues/QueueInterface.java
@@ -0,0 +1,9 @@
+package lab3Queues;
+
+public interface QueueInterface <T> {
+    public void enqueue (T newEntry);
+    public T dequeue ();
+    public T getFront();
+    public boolean isEmpty();
+    public void clear();
+}
