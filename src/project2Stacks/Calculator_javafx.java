c24b3b387084c32e89597cd6f66e7c33990cafed
TechnoHound <ajkreuzkamp@gmail.com>
2019-10-04 12:23:24 -0400

    not ready yet, but wanted to save my progress

--- src/project2Stacks/Calculator_javafx.java
+++ src/project2Stacks/Calculator_javafx.java
@@ -1,6 +1,36 @@
 package project2Stacks;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
 public class Calculator_javafx
 {
+	
+	public void start(Stage primaryStage) throws Exception
+	{
+		VBox complete = new VBox();
+		complete.setPrefWidth(300);
+		complete.setPrefHeight(410);
+		complete.setAlignment(Pos.TOP_CENTER);
+		complete.setPadding(new Insets(10,10,10,10));
+		
+			TextField screen = new TextField();
+			complete.getChildren().add(screen);
+			screen.setPrefHeight(60);
+			
+			VBox buttons = new VBox();
+			complete.getChildren().add(buttons);
+			buttons.setPrefHeight(320);
+			buttons.setAlignment(Pos.TOP_CENTER);
+		
+		Scene scene = new Scene(complete, 300, 410);
+		primaryStage.setTitle("CALCULATOR ~ AJ, John, & Tony");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
 	
 }
