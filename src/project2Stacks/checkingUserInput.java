fd209e2dacc593a716138efbe6ff94d54c519c9f
John Li <lij19@wit.edu>
2019-10-09 18:03:33 -0400

    Update checkingUserInput.java

--- src/project2Stacks/checkingUserInput.java
+++ src/project2Stacks/checkingUserInput.java
@@ -1,9 +1,6 @@
 package project2Stacks;
 
-public class CalculatingResult {
-	public int calculatingAnswer(String equation) { //will take in the equation and calculate the correct answer
-		return 0; //a placeholder for now
-	}
+public class checkingUserInput {
 	
 	public static boolean balanceChecker(String equation) {
 		LinkedStack<Character> paranthesis = new LinkedStack<>();
@@ -24,11 +21,7 @@ public class CalculatingResult {
 		} else {
 			return false;
 		}
-	}
-	public static boolean isPaired (char first, char second) {
-		return true; //placeholder for now
-	}
-	public static String convert(String a) {
+	}public static String convert(String a) {
 		return ""; //placeholder for now
 	}
 	public static int precedence(char c) {
