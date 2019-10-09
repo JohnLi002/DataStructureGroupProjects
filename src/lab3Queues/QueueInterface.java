ee74edf5b5dc96f114ccd143a5e4bcb0f6054464
John Li <lij19@wit.edu>
2019-10-09 16:27:20 -0400

    added comments to QueueInterface.java

--- src/lab3Queues/QueueInterface.java
+++ src/lab3Queues/QueueInterface.java
@@ -5,9 +5,15 @@ package lab3Queues;
  */
 
 public interface QueueInterface <T> {
-    public void enqueue (T newEntry);
-    public T dequeue ();
-    public T getFront();
-    public boolean isEmpty();
-    public void clear();
+	
+	public void enqueue (T newEntry); //adds the object newEntry to the back of the queue
+	
+    public T dequeue (); //gets rid of the object at the front of the queue and returns said removed object
+    
+    public T getFront(); //returns what is at the front of the queue
+    
+    public boolean isEmpty(); //checks and returns whether or not something is empty
+    
+    public void clear(); //clears everything
+    
 }
