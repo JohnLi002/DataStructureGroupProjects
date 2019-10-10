a8292a23482f2d936109087ab75065fd3ea4cbec
John Li <lij19@wit.edu>
2019-10-10 16:56:33 -0400

    Finished the CheckingUserInput.java which converts infix to postfix for calculating results

--- src/project2Stacks/CheckingUserInput.java
+++ src/project2Stacks/CheckingUserInput.java
@@ -22,9 +22,63 @@ public class CheckingUserInput {
 			return false;
 		}
 	}public static String convert(String a) {
-		return ""; //placeholder for now
+		String result = "";
+		LinkedStack<Character> operators = new LinkedStack<>();
+		boolean numDigit = true; //when operators are added last, this boolean will make sure a single space is inputted
+		
+		while(!a.isEmpty()) {
+			char c = a.charAt(0);
+			a = a.substring(1, a.length());
+			
+			if(c >= '0' && c <= '9') {
+				result += c;
+			} else if(c == '*' || c == '/' || c == '+' || c== '-' || c== '^') {
+				while(!operators.isEmpty() && precedence(c) <= precedence(operators.peek())) { //makes sure that all numbers with greater or equal importance is thrown into the equation first
+					result += operators.pop();
+				}
+				
+				result += " "; //an operator means that the number has ended
+				
+				operators.push(c);
+			} else if(c == '(') {
+				operators.push(c);
+			} else if(c == ')') {
+				while(!(operators.peek() == '(')) {
+					result += operators.pop();
+				}
+				operators.pop(); //gets rid of remaining '('
+			} else {
+				continue; //this statement is just to let the user know that there is nothing else to check for
+			}
+		}
+		
+		while(!operators.isEmpty()) { //just in case there is a single operator left in the stack
+			if(numDigit) {
+				result += " ";
+				numDigit = false;
+			}
+			result+=operators.pop();
+		}
+		
+		return result;
 	}
-	public static int precedence(char c) {
-		return 0; //placeholder for now
-	}
+	
+	private static int precedence(char c) {
+			if(c== '(') {
+				return 1;
+			} else if(c == ')') {
+				return 1;
+			} else if(c == '+') {
+				return 2;
+			} else if(c == '-') {
+				return 2;
+			} else if(c == '*') {
+				return 3;
+			} else if(c == '/') {
+				return 3;
+			} else { //must be '^'
+				return 4;
+			}
+		}
+	
 }
--- src/project2Stacks/Test.java
+++ src/project2Stacks/Test.java
@@ -2,6 +2,6 @@ package project2Stacks;
 
 public class Test {
 	public static void main(String[] args) {
-		
+		System.out.println(CheckingUserInput.convert("(12+32)*(32*3)"));
 	}
 }
