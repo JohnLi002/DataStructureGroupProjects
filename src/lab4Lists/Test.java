3977fb4ef8434f95799ae37408b40c706298cee4
Holyglade <39798943+Holyglade@users.noreply.github.com>
2019-10-15 18:33:15 -0400

    foxandcat.txt is readable.

--- src/lab4Lists/Test.java
+++ src/lab4Lists/Test.java
@@ -2,6 +2,12 @@ package lab4Lists;
 
 public class Test {
 	public static void main(String[] args) {
+		try {
+		
+		}	
+		catch(FileNotFoundException ex) {
+			System.out.println ("")
+		}
 		
 	}
 }
