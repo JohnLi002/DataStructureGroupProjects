328d08eec65cb3463cd402183137af53babfb9fb
Holyglade <39798943+Holyglade@users.noreply.github.com>
2019-10-15 18:34:44 -0400

    again

--- src/lab4Lists/Test.java
+++ src/lab4Lists/Test.java
@@ -1,12 +1,17 @@
 package lab4Lists;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Test {
 	public static void main(String[] args) {
 		try {
-		
+		File foxandcat = new File("foxandcat.txt");
+		Scanner sc = new Scanner(foxandcat);
 		}	
 		catch(FileNotFoundException ex) {
-			System.out.println ("")
+			System.out.println ("foxandcat.txt not found");
 		}
 		
 	}
