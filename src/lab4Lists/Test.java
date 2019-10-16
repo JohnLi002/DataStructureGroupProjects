b0e33b9098ee6f1730d23ed1e55d7a641c5b5706
TechnoHound <ajkreuzkamp@gmail.com>
2019-10-16 11:33:26 -0400

    groundwork

--- src/lab4Lists/Test.java
+++ src/lab4Lists/Test.java
@@ -7,19 +7,15 @@ import java.util.Scanner;
 public class Test {
 	public static void main(String[] args) {
 		LinkedList<String> list = new LinkedList<>();
-		list.add("2");
-		list.add("3");
-		list.add("4");
-		list.add("5");
-		list.add("6");
-		list.add("7");
-		
-		System.out.println(list.remove("2"));
-		System.out.println(list.getEntry(0));
 		
 		try {
 			File foxandcat = new File("foxandcat.txt");
 			Scanner sc = new Scanner(foxandcat);
+			
+			while (sc.hasNext())
+			{
+				
+			}
 			
 		} catch(FileNotFoundException ex) {
 			System.out.println ("foxandcat.txt not found");
