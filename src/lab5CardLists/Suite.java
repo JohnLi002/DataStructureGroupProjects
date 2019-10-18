9d0b0ef6cd83eb3bb1816adfff5a7a42f1a6d2f4
TechnoHound <ajkreuzkamp@gmail.com>
2019-10-18 10:41:01 -0400

    FIXED SUITE

--- src/lab5CardLists/Suite.java
+++ src/lab5CardLists/Suite.java
@@ -1,19 +1,5 @@
 package lab5CardLists;
 
-public class Suite {
-	private String suites;
-
-	public static Suite valueOf(String s) {
-		Suite result = new Suite();
-		result.setSuites(s);
-		return result;
-	}
-	public String getSuites() {
-		return suites;
-	}
-
-	public void setSuites(String suites) {
-		this.suites = suites;
-	}
-	
+public enum Suite {
+	Spades, Diamonds, Clubs, Hearts
 }
