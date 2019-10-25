5859d0a70784d145648f920d8814afe80eecc897
TechnoHound <ajkreuzkamp@gmail.com>
2019-10-25 11:48:51 -0400

    apparently I've done something

--- src/project3Queues/Simulation.java
+++ src/project3Queues/Simulation.java
@@ -2,10 +2,15 @@ package project3Queues;
 
 public class Simulation {
 	public static int runTimeMinutes = 60;
-	public Train t1 = new Train(0, false);
-	public Train t2 = new Train(10, true);
 	
 	public static void main(String[] args) {
+		Route orangeLine = new Route("orange.txt");
+		orangeLine.initializeStations();
+		
+		Train t1 = new Train(0, false);
+		Train t2 = new Train(10, true);
+		
+		//	HOW TIME PASSES
 		for (int i = 0; i <= runTimeMinutes; i++) {
 			
 		}
