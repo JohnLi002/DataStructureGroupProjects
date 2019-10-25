c765e8a2f28840b98a9c78b9a5682a7f30892bc2
John Li <lij19@wit.edu>
2019-10-25 11:33:26 -0400

    Update Train.java

--- src/project3Queues/Train.java
+++ src/project3Queues/Train.java
@@ -3,17 +3,20 @@ package project3Queues;
 public class Train {
 	private int currentIndex;
 	//private BagInterface<Passsenger> trainPassengers;
-	private int direction;
+	private boolean direction;
 	private int maxTime = 0;
+	
+	public Train(int time) {
+		maxTime = time;
+	}
+	
 	public int getCurrentIndex() {
 		return currentIndex;
 	}
-	/*public Train(int time) {
-		maxTime = time;
-	}
+	
 	//this method will be the train moving from station to another
 	public void trainMove(int transportTime) {
 		int time = transportTime;
 		
-	}*/
+	}
 }
