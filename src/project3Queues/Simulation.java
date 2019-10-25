57e19881b1967af97c1dea53aefea93597abbaf5
TechnoHound <ajkreuzkamp@gmail.com>
2019-10-25 11:56:37 -0400

    did things

--- src/project3Queues/Simulation.java
+++ src/project3Queues/Simulation.java
@@ -5,13 +5,17 @@ public class Simulation {
 	
 	public static void main(String[] args) {
 		Route orangeLine = new Route("orange.txt");
-		orangeLine.initializeStations();
 		
 		Train t1 = new Train(0, false);
 		Train t2 = new Train(10, true);
 		
 		//	HOW TIME PASSES
 		for (int i = 0; i <= runTimeMinutes; i++) {
+			//every 10 minutes fill stations with passengers
+			if (runTimeMinutes % 10 == 0)
+			{
+				orangeLine.initializeStations();
+			}
 			
 		}
 	}
