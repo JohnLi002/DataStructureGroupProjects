b7c736fa8982a2786c1aa6a905afc567f6d47ab7
Holyglade <39798943+Holyglade@users.noreply.github.com>
2019-10-25 23:53:10 -0400

    gibberish

--- src/project3Queues/Test.java
+++ src/project3Queues/Test.java
@@ -1,18 +1,27 @@
 package project3Queues;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("==============ROUTE CLASS=============");
 		Route orangeLine = new Route("orange.txt");
 		Train t1 = new Train(0, false);
-		
-		orangeLine.initializeStations();
-		Station[] station = orangeLine.getStation();
-		for(int i = 0; i < 17; i++) {
+		try {
+			orangeLine.initializeStations();
+			Station[] station = orangeLine.getStation();
+			for(int i = 0; i < 17; i++) {
+				t1.removePassengers(station[i]);
+			}
 			
+			orangeLine.print();
+			t1.print();
+			PrintWriter printer = new PrintWriter("trains.txt");
+			printer.println(orangeLine.travel(t1));
+		} catch(FileNotFoundException ex) {
+			System.out.println ("trains.txt not found");
 		}
-		t1.removePassengers(station[0]);
-		orangeLine.print();
-		t1.print();
+		
 	}
 }
