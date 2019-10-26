e11269b469e2277dcd4033b6c6d00544706d0a72
John Li <lij19@wit.edu>
2019-10-26 00:02:50 -0400

    Update Train.java

--- src/project3Queues/Train.java
+++ src/project3Queues/Train.java
@@ -19,18 +19,18 @@ public class Train {
 
 	//this method will be the train moving from station to another
 	public void trainMove(int max) {
-		if (goingForward) {
-			currentIndex++;
-		}
-		else if (!goingForward) {
-			currentIndex--;
-		}
 		if(currentIndex == 0) {
 			goingForward = true;
 		} else if (currentIndex == max) {
 			goingForward = false;
 		}
 		
+		if (goingForward) {
+			currentIndex++;
+		}
+		else if (!goingForward) {
+			currentIndex--;
+		}
 	}
 
 	public void addPassenger(Passenger p) {
