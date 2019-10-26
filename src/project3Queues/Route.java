3f60935de850c9a4a4e502f16a72fc1f2c24844c
Holyglade <39798943+Holyglade@users.noreply.github.com>
2019-10-25 23:19:30 -0400

    something

--- src/project3Queues/Route.java
+++ src/project3Queues/Route.java
@@ -104,4 +104,8 @@ public class Route {
 	public void travel() {
 		
 	}
+	
+	public Station[] getStation() {
+		return stations;
+	}
 }
--- src/project3Queues/Test.java
+++ src/project3Queues/Test.java
@@ -4,7 +4,15 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("==============ROUTE CLASS=============");
 		Route orangeLine = new Route("orange.txt");
+		Train t1 = new Train(0, false);
+		
 		orangeLine.initializeStations();
+		Station[] station = orangeLine.getStation();
+		for(int i = 0; i < 17; i++) {
+			
+		}
+		t1.removePassengers(station[0]);
 		orangeLine.print();
+		t1.print();
 	}
 }
