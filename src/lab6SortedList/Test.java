8cdc8f6d20de22b2cfa151fbf2f8b30d65591117
Holyglade <39798943+Holyglade@users.noreply.github.com>
2019-10-28 21:57:03 -0400

    Updated Message

--- src/lab6SortedList/Test.java
+++ src/lab6SortedList/Test.java
@@ -1,5 +1,12 @@
 package lab6SortedList;
 
-public class Test {
 
+
+public class Test {
+	public static void main(String[] args) {
+		Message m1 = new Message("hey");
+		Object[] p1 = m1.encode();
+		System.out.println();
+		
+	}
 }
