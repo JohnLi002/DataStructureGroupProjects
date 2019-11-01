1258431caaf003af22ca27a2670002779d55a473
John Li <lij19@wit.edu>
2019-11-01 11:21:07 -0400

    added more to deck

--- src/project4Lists/Deck.java
+++ src/project4Lists/Deck.java
@@ -1,17 +1,30 @@
 package project4Lists;
 
 public class Deck extends Pile{
+
 	
 	public Deck() {
-		createDeck();
+		this(false);
 	}
 	
+	public Deck(boolean b) {
+		createDeck();
+		if(b) {
+			shuffle();
+		}
+	}
+
+	public Card draw() {
+		Card c = getFirstNode().getCard(); //gets the card at the top of the deck
+		remove(c); //remove said pile
+		return c;
+	}
+
 	private void createDeck() {
-		for(Suite s : Suite.values()) {
+		for(Suite s : Suite.values()) { //creates a standards unique 52 deck of playing cards
 			for(Rank r : Rank.values()) {
-				super.add(new Card(s.toString(), r.toString()));
+				add(new Card(s, r));
 			}
 		}
-		
 	}
 }
