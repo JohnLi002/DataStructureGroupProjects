532c67a0e656797bcd9d2c0e9fd54e51c10ee75b
John Li <lij19@wit.edu>
2019-11-04 10:19:06 -0500

    Adding dictionary notes

---
+++ notes/dictionary/Name.java
@@ -0,0 +1,26 @@
+package dictionary;
+
+import java.util.Scanner;
+
+public class Name {
+	String firstName;
+	String lastName;
+	
+	public Name(String first, String last) {
+		
+	}
+	
+	public Name(String full) {
+		
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		
+		return false;
+	}
+	
+	public void readFile(Scanner data) {
+		
+	}
+}
