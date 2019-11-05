4b658b6191d8ff54e9b00a84d2603cbe9e0890f2
John Li <lij19@wit.edu>
2019-11-05 10:00:32 -0500

    added setter and getter methods in Line2d

--- src/lab7DoublyLinkedList/Line2d.java
+++ src/lab7DoublyLinkedList/Line2d.java
@@ -3,4 +3,20 @@ package lab7DoublyLinkedList;
 public class Line2d {
 	private int Start;
 	private int End;
+	
+	public int getStart() {
+		return Start;
+	}
+	
+	public void setStart(int start) {
+		Start = start;
+	}
+	
+	public int getEnd() {
+		return End;
+	}
+	
+	public void setEnd(int end) {
+		End = end;
+	}
 }
