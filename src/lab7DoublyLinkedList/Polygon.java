c1a000c63aa913224684ca1095550c285d974a71
John Li <lij19@wit.edu>
2019-11-06 23:15:53 -0500

    Update Polygon.java

--- src/lab7DoublyLinkedList/Polygon.java
+++ src/lab7DoublyLinkedList/Polygon.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-import iteratorDoubleLinkedList.ListIterator;
 
 public class Polygon {
 	DoublyLinkedList<Line> polygon;
