45cf8978638d7259599a03db185a1529961a9319
TechnoHound <ajkreuzkamp@gmail.com>
2019-11-08 11:37:14 -0500

    added a GUI.java class

---
+++ src/project4Lists/GUI.java
@@ -0,0 +1,16 @@
+package project4Lists;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class GUI extends Application
+{
+	
+	public void start(Stage primaryStage) {
+		
+	}
+	public static void main(String[] args)
+	{
+		launch(args);
+	}
+}
