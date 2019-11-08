326319a30ea16a961b9aa923a8aee0875b377f30
TechnoHound <ajkreuzkamp@gmail.com>
2019-11-08 11:51:43 -0500

    more basic GUI stuff I want to save

--- src/project4Lists/GUI.java
+++ src/project4Lists/GUI.java
@@ -1,13 +1,23 @@
 package project4Lists;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class GUI extends Application
 {
 	
-	public void start(Stage primaryStage) {
+	public void start(Stage primaryStage) throws Exception
+	{
+		VBox complete = new VBox();
 		
+		
+		Scene scene = new Scene(complete, 400, 600);
+		primaryStage.setTitle("Blackjack");
+		primaryStage.setScene(scene);
+		primaryStage.show();
 	}
 	public static void main(String[] args)
 	{
