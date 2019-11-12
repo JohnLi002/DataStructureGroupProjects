97a44a355b9dc75ab0b22a33b4f76743b6db12b0
John Li <lij19@wit.edu>
2019-11-12 13:34:34 -0500

    added method descriptions to Card.java

--- src/project4Lists/Card.java
+++ src/project4Lists/Card.java
@@ -18,26 +18,57 @@ public class Card {
 		rank = r;
 	}
 
+	/**
+	 * Simply returns what is the enem Suite of this card
+	 * 
+	 * @return returns the suite of this card
+	 */
 	public Suite getSuite() {
 		return suite;
 	}
-
+	
+	/**
+	 * Sets the suite of this card to the suite given in the parameters
+	 * 
+	 * @param suite Suite to be set
+	 */
 	public void setSuite(Suite suite) {
 		this.suite = suite;
 	}
-
+	
+	/**
+	 * Simply returns what is the enem Rank of this card
+	 * 
+	 * @return returns the rank of this card
+	 */
 	public Rank getRank() {
 		return rank;
 	}
-
+	
+	/**
+	 * Sets the rank of this card to the rank given in the parameters
+	 * 
+	 * @param rank Rank to be set
+	 */
 	public void setRank(Rank rank) {
 		this.rank = rank;
 	}
 	
+	/**
+	 * Returns whether or not this card and the other card given in the param are the same
+	 * 
+	 * @param c the other card being compared
+	 * @return boolean of whether or not the two cards are the same.
+	 */
 	public boolean equals(Card c) {
 		return (getSuite().equals(c.getSuite()) && getRank().equals(c.getRank()));
 	}
 	
+	/**
+	 * returns the card in a string format
+	 * 
+	 * @return returns the string of this card
+	 */
 	public String toString() {
 		return getRank() + " OF " + getSuite();
 	}
