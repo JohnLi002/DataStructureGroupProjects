cfbae3795a81dc7cf7238c6f3d498cb6d724fe0d
TechnoHound <ajkreuzkamp@gmail.com>
2019-11-25 11:37:30 -0500

    did part 2 and got started on part 3

--- src/lab9Trees/Parts2and3.java
+++ src/lab9Trees/Parts2and3.java
@@ -21,7 +21,31 @@ public class Parts2and3 {
 
 	public static void main(String[] args)
 	{
+		//	START Part 2
+		BinaryTree<String> BT_L = new BinaryTree<String> ("L", null, null);
+		BinaryTree<String> BT_I = new BinaryTree<String> ("I", null, BT_L);
+		BinaryTree<String> BT_H = new BinaryTree<String> ("H", null, null);
+		BinaryTree<String> BT_D = new BinaryTree<String> ("D", BT_H, BT_I);
+		BinaryTree<String> BT_E = new BinaryTree<String> ("E", null, null);
+		BinaryTree<String> BT_B = new BinaryTree<String> ("B", BT_D, BT_E);
 		
+		BinaryTree<String> BT_J = new BinaryTree<String> ("J", null, null);
+		BinaryTree<String> BT_K = new BinaryTree<String> ("K", null, null);
+		BinaryTree<String> BT_G = new BinaryTree<String> ("G", BT_J, BT_K);
+		BinaryTree<String> BT_F = new BinaryTree<String> ("F", null, null);
+		BinaryTree<String> BT_C = new BinaryTree<String> ("C", BT_F, BT_G);
+		
+		BinaryTree<String> BT_A = new BinaryTree<String> ("A", BT_B, BT_C);
+			//NOTE: BT_A is the binary tree corresponding to the prompted diagram
+		//	END Part 2
+//		//		//		//		//		//		//		//		//		//		//		//
+		//	START Part 3
+		System.out.println("_Prompted Tree_");
+		System.out.print("  height: " + BT_A.getHeight() );
+		System.out.print("  number of nodes: " + BT_A.getNumberOFNodes() );
+		System.out.print("  leaves: ");
+		System.out.print("  four tree traversals: ");
+		//	END Part 3
 	}
 
 }
